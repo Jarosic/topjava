9da08bafee48e7e4f7762a91984e83b48e0b6176
Rybalka Yaroslav <jaroslav.rybalka@ukr.net>
2021-03-29 20:22:49 +0300

    Create root controller test for meals

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/JspMealControllerTest.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JspMealControllerTest extends AbstractControllerTest {
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void updateOrCreate() {
+    }
+
+    @Test
+    void getBetween() {
+    }
+}
