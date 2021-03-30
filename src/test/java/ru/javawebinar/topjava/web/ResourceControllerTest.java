52698bbd406f8fd3465325228df744a3f26822e2
Rybalka Yaroslav <jaroslav.rybalka@ukr.net>
2021-03-30 21:31:32 +0300

    Create root controller test for MealRestController

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class ResourceControllerTest extends AbstractControllerTest {
+    private final List<String> attr = new ArrayList<>();
+
+
+    @Test
+    void getStyle() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css;charset=UTF-8"));
+    }
+}
